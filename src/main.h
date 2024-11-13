24c6b08bd82d476bc6caf384915be9d758c85867
Svolle <svolle777@icloud.com>
2024-11-13 14:00:46 +0100

    Changes in box_message function.

--- src/main.h
+++ src/main.h
@@ -4,5 +4,5 @@ typedef enum {
     ja
 } preference_e;
 
-const char MESSAGE[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut egestas nec nisi in ultrices. Sed enim magna, cursus mattis felis vitae, sodales mollis turpis. Quisque nec libero malesuada, lobortis libero in, condimentum ipsum. Vestibulum venenatis, massa in aliquet viverra, quam neque pulvinar leo, ut rutrum sapien tortor a ante. Sed.";
+const char MESSAGE[] = "Andersen's fairy tales, consisting of 156 stories across nine volumes, have been translated into more than 125 languages. They have become embedded in Western collective consciousness, accessible to children as well as presenting lessons of virtue and resilience in the face of adversity for mature readers.";
 #endif //MAIN_H
--- src/terminal.c
+++ src/terminal.c
@@ -4,29 +4,29 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "general.h"
+
 void box_message(const char message[]) {
     const int messageLength = strlen(message);
-    const int messagePadding = 0;
-    const int boxWidth = 32;
-    const int boxHeight = ceil(messageLength / (double) (boxWidth-2));
+    const int messagePadding = 3;
+    const int boxWidth = 20;
+    const int boxHeight = ceil(messageLength / (double) boxWidth);
 
-    int breaks[boxHeight-1];
-    int breakIndex = boxWidth;
+    int breaks[boxHeight - 1];
+    int breakIndex = boxWidth - 1;
     for (int n = 0; n < boxHeight; n++) {
-        int x = 0;
-        while (message[breakIndex] != ' ') {
+        while (message[breakIndex] != ' ' && breakIndex >= 0) {
             breakIndex--;
-            x ++;
         }
 
         breaks[n] = breakIndex;
         printf("%d\n", breakIndex);
-        breakIndex += boxWidth+x;
+        breakIndex += boxWidth;
     }
 
     // Top side of text box.
     printf("%c", 201);
-    for (int i = 0; i < boxWidth+2*messagePadding-2; i++) {
+    for (int i = 0; i < boxWidth + 2 * messagePadding; i++) {
         printf("%c", 205);
     }
     printf("%c\n", 187);
@@ -35,29 +35,30 @@ void box_message(const char message[]) {
 
     // Print box sides.
     for (int i = 0; i < boxHeight; i++) {
-        for (int j = 0; j < boxWidth; j++) {
-            if (j == 0) {
+        for (int j = -1; j <= boxWidth; j++) {
+            if (j == -1) {
+                // Print left side of box plus padding.
                 printf("%c", 186);
                 for (int k = 0; k < messagePadding; k++) {
                     printf(" ");
                 }
-            } else if (j == boxWidth - 1) {
+            } else if (j == boxWidth) {
+                // Print padding plus right side of box.
                 for (int k = 0; k < messagePadding; k++) {
                     printf(" ");
                 }
                 printf("%c\n", 186);
             } else {
                 if (n <= messageLength) {
-                   if (n < breaks[i]) {
+                    if (n < breaks[i]) {
                         printf("%c", message[n]);
                         n++;
-                   } else if (message[n] == ' ') {
-                        n++;
-                   } else {
+                    } else if (message[n] == ' ') {
                         printf(" ");
-                   }
-                } else {
-                    printf(" ");
+                        n++;
+                    } else {
+                        printf(" ");
+                    }
                 }
             }
         }
@@ -65,7 +66,7 @@ void box_message(const char message[]) {
 
     // Bottom side of text box.
     printf("%c", 200);
-    for (int i = 0; i < boxWidth+2*messagePadding-2; i++) {
+    for (int i = 0; i < boxWidth + 2 * messagePadding; i++) {
         printf("%c", 205);
     }
     printf("%c", 188);
