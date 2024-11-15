44a488da543596a1803aa4468df899382a27e8c6
Svolle <svolle777@icloud.com>
2024-11-15 12:08:42 +0100

    Temporary input testing.

--- src/main.c
+++ src/main.c
@@ -1,35 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <math.h>
 #include <string.h>
 
 #include "main.h"
 #include "dataHandling.h"
 #include "terminal.h"
+#include "journey.h"
 
 void print_introduction(const char message[]);
-void get_journey(location_s *start, location_s *end);
-void get_preferences(void);
-void get_result(void);
-void print_table(void);
+
+HANDLE hstdin;
+DWORD mode;
 
 int main(void) {
     location_s startLocation, endLocation;
-    preference_e enviormentPreference;
+    preference_e environmentPreference;
 
-    box_message(MESSAGE);
+    // Print instructions.
+    box_message(introduction, "Welcome");
 
-    // print_introduction();
+    box_message("", "Start");
+
+    hstdin = GetStdHandle(STD_INPUT_HANDLE);
+
+    GetConsoleMode(hstdin, &mode);
+    SetConsoleMode(hstdin, mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT));
+
+    printf("\033[7;3H");
+
+    int i = 0;
+    char c;
+    while (i <= 32) {
+        printf("\033[5;%dH", i + 3);
+        printf("");
+        scanf("%c", &c);
+
+        if (c == 8) {
+            i--;
+        } else {
+            printf("%c", c);
+            i++;
+        }
+    }
+    printf("\033[2E");
+
+    box_message("", "Destination");
 
     // get_journey(&startLocation, &endLocation);
 
     // get_preferences();
-    
+
     // get_result();
-    
+
     // print_table();
 
-    // while (1) {}
+    while (1) {
+    }
 
     return EXIT_SUCCESS;
 }
--- src/main.h
+++ src/main.h
@@ -4,5 +4,6 @@ typedef enum {
     ja
 } preference_e;
 
+const char introduction[] = "Please enter start location and destination.";
 const char MESSAGE[] = "Andersen's fairy tales, consisting of 156 stories across nine volumes, have been translated into more than 125 languages. They have become embedded in Western collective consciousness, accessible to children as well as presenting lessons of virtue and resilience in the face of adversity for mature readers.";
 #endif //MAIN_H
