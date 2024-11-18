8afa5520dd32287ed2924d564138030bb076a6dd
Svolle <svolle777@icloud.com>
2024-11-18 11:58:55 +0100

    Changed 'box_message' to 'box_print' and added 'box_read' function.

--- src/terminal.h
+++ src/terminal.h
@@ -1,9 +1,17 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
-void box_message(const char message[], const char title[]);
+void box_print(const char message[], const char title[]);
 
-int get_length_of_longest_word(const char message[]);
+char *box_read();
+
+void print_top_of_box(int width, const char title[]);
+
+void print_bottom_of_box(int width);
+
+int length_of_longest_word(const char message[]);
+
+int number_of_words(const char message[]);
 
 void print_result();
 
