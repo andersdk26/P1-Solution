dc15926e8e3e8656b51d8601c8df23446bcbffde
Svolle <svolle777@icloud.com>
2024-11-22 16:25:27 +0100

    Bug fixes and function improvements.

--- src/terminal.c
+++ src/terminal.c
@@ -34,68 +34,94 @@ void box_print(const char message[], const char title[]) {
             breakIndex--;
         }
 
+
         breaks[n] = breakIndex;
-        //printf("%d\n", breaks[n]);
+        printf("%d\n", breakIndex);
         breakIndex += BOX_WIDTH;
     }
 
     // Top side of text box.
     print_top_of_box(title);
 
-    int n = 0;
-
+    int m = 0;
     // For every line of text in the box.
     for (int i = 0; i < boxHeight; i++) {
-        // For the width of the box.
-        for (int j = 0; j < BOX_WIDTH; j++) {
-            // If it's the first iteration, print the left side of the box.
-            set_win_color(wc_gray);
-            if (j == 0) {
-                printf("%c", 186);
-                for (int k = 0; k < BOX_PADDING; k++) {
-                    printf(" ");
-                }
+        // For the left side, middle and right side of the box.
+        for (int j = 0; j < 3; j++) {
+            switch (j) {
+                case 0: // Print left side of the box.
+                    set_win_color(wc_gray);
+                    printf("%c", 186);
+                    for (int k = 0; k < BOX_PADDING; k++) {
+                        printf(" ");
+                    }
+                    break;
+                case 1: // Print line of text.
+                    set_win_color(wc_bright_white);
+                    int n = 0;
+                    while (n < BOX_WIDTH) {
+                        if (BOX_WIDTH * i + n - m < messageLength) {
+                            if (n <= breaks[i]) {
+                                printf("%c", message[BOX_WIDTH * i + n - m]);
+                            } else {
+                                printf("^");
+                                m++;
+                            }
+                        } else {
+                            printf("_");
+                        }
+
+                        if (BOX_WIDTH * i + n - m >= messageLength) {
+                            i = boxHeight - 1;
+                        }
+                        n++;
+                    }
+                    break;
+                case 2: // Print right side of the box.
+                    set_win_color(wc_gray);
+                    for (int k = 0; k < BOX_PADDING; k++) {
+                        printf(" ");
+                    }
+                    printf("%c\n", 186);
+                    break;
+                default:
+                    printf("Error\n");
+                    exit(EXIT_FAILURE);
             }
 
+            /*
             // Otherwise, print characters from message.
             set_win_color(wc_bright_white);
             if (n < messageLength) {
                 if (n <= breaks[i]) {
-                    //printf("%d", n%16);
-                    printf("%c", message[n]);
+                    printf("%d", n);
+                    if (n % BOX_WIDTH == 0 && message[n] == ' ') {
+                    } else {
+                        printf("%c", message[n]);
+                    }
                     n++;
                 } else {
-                    printf(" ");
-                    n = breaks[i] + 1;
+                    printf("^");
+                    //n = breaks[i] + 1;
                 }
             } else {
                 // If end of message has been reached, then print empty spaces.
-                printf(" ");
+                printf("_");
             }
-
-            // If it's the last iteration, print the right side of the box.
-            set_win_color(wc_gray);
-            if (j == BOX_WIDTH - 1) {
-                for (int k = 0; k < BOX_PADDING; k++) {
-                    printf(" ");
-                }
-                printf("%c\n", 186);
-            }
+            */
         }
 
         // If end of message has been reached, break out of the for-loop.
-        if (n == messageLength) {
-            break;
-        }
+        //if (n == messageLength) {
+        //    break;
+        //}
     }
 
     // Bottom side of text box.
     print_bottom_of_box(BOX_WIDTH + 2 * BOX_PADDING);
 }
 
-char *box_read(const char title[], const enum inputType type) {
-    // TODO: Fjern type parameter siden 'box_read' kun bliver brugt til start og destination.
-
+char *box_read(const char title[]) {
     // Print the top of the box.
     print_top_of_box(title);
 
@@ -109,7 +135,7 @@ char *box_read(const char title[], const enum inputType type) {
     // Print the bottom of the box.
     print_bottom_of_box();
 
-    // Move the cursor 2 rows up and 1 + padding columns to the right.
+    // Move the cursor into the box.
     printf("\033[2A");
     printf("\033[%dC", 1 + BOX_PADDING);
 
@@ -118,35 +144,10 @@ char *box_read(const char title[], const enum inputType type) {
     GetConsoleMode(hstdin, &mode);
     SetConsoleMode(hstdin, mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT));
 
-    int i = 0;
-    char c;
     char *input = malloc(BOX_WIDTH + 1);
     set_win_color(wc_bright_white);
+    read_characters(input);
 
-    while (1) {
-        scanf("%c", &c);
-
-        if (i > 0) {
-            if (c == 8) {
-                printf("\033[1D");
-                printf(" ");
-                printf("\033[1D");
-                input[i] = '\0';
-                i--;
-            } else if (c == 46) {
-                break;
-            }
-        }
-
-        if (i < BOX_WIDTH && c != 8) {
-            if (type == STRING && isalpha(c) || type == NUMBER && isdigit(c)) {
-                printf("%c", c);
-                input[i] = c;
-                i++;
-            }
-        }
-    }
-    input[i + 1] = '\0';
     printf("\033[2E");
     set_win_color(wc_gray);
     return input;
@@ -156,8 +157,69 @@ void get_priorities(char *price, char *time, char *emission) {
     const char titles[3][10] = {{"Time"}, {"Price"}, {"Emission"}};
 
     print_top_of_priority_boxes(titles);
-
+    print_middle_of_priority_boxes(titles);
     print_bottom_of_priority_boxes(titles);
+
+    printf("\033[2A");
+    printf("\033[3C");
+    set_win_color(wc_bright_white);
+
+    char c = '\0';
+    while (c < 49 || c > 51) {
+        scanf(" %c", &c);
+    }
+
+    *price = c;
+    printf("%c", c);
+    c = '\0';
+
+    printf("\033[9C");
+
+    while (c < 49 || c > 51 || c == *price) {
+        scanf(" %c", &c);
+    }
+
+    *time = c;
+    printf("%c", c);
+    c = '\0';
+
+    printf("\033[10C");
+
+    while (c < 49 || c > 51 || c == *price || c == *time) {
+        scanf(" %c", &c);
+    }
+
+    *emission = c;
+    printf("%c", c);
+
+    printf("\033[2B");
+    printf("\033[0G");
+    set_win_color(wc_gray);
+}
+
+void read_characters(char *input) {
+    // TODO: Fix warning?
+    int i = 0;
+    char c;
+    while (1) {
+        scanf(" %c", &c);
+        if (i > 0) {
+            if (c == 8) {
+                printf("\033[1D \033[1D");
+                input[i] = '\0';
+                i--;
+            } else if (c == 46) {
+                break;
+            }
+        }
+
+        if (i < BOX_WIDTH && isalpha(c)) {
+            printf("%c", c);
+            input[i] = c;
+            i++;
+        }
+    }
+    input[i] = '\0';
 }
 
 void print_top_of_priority_boxes(const char titles[3][10]) {
@@ -170,6 +232,17 @@ void print_top_of_priority_boxes(const char titles[3][10]) {
         printf("\033[0m");
         set_win_color(wc_gray);
         printf(" %c%c", 198, 187);
+    }
+    printf("\n");
+}
+
+void print_middle_of_priority_boxes(const char titles[3][10]) {
+    for (int i = 0; i < 3; i++) {
+        printf("%c", 186);
+        for (int j = 0; j < strlen(titles[i]) + 4; j++) {
+            printf(" ");
+        }
+        printf("%c", 186);
     }
     printf("\n");
 }
--- src/terminal.h
+++ src/terminal.h
@@ -5,11 +5,15 @@ enum inputType {STRING, NUMBER};
 
 void box_print(const char message[], const char title[]);
 
-char *box_read(const char title[], enum inputType);
+char *box_read(const char title[]);
 
 void get_priorities(char *price, char *time, char *emission);
 
+void read_characters(char *input);
+
 void print_top_of_priority_boxes(const char titles[3][10]);
+
+void print_middle_of_priority_boxes(const char titles[3][10]);
 
 void print_bottom_of_priority_boxes(const char titles[3][10]);
 
