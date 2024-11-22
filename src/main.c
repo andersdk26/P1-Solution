7d4ef605384dd6fb77def80a8f37293b8600aabe
Svolle <svolle777@icloud.com>
2024-11-22 16:24:53 +0100

    Trivial changes.

--- src/main.c
+++ src/main.c
@@ -17,11 +17,11 @@ int main(void) {
     box_print(journeyInstructions, "Journey");
 
     // Get start location.
-    char *inputStart = box_read("Start", STRING);
+    char *inputStart = box_read("Start");
     check_input(inputStart);
 
     // Get destination.
-    char *inputDestination = box_read("Destination", STRING);
+    char *inputDestination = box_read("Destination");
     check_input(inputDestination);
 
     get_train_routes(inputStart, inputDestination, &routes, &routeQuantity);
@@ -34,31 +34,11 @@ int main(void) {
     // Print prioritisation instructions.
     box_print(prioritisationInstructions, "Prioritisation");
 
-    char a;
-    char b;
-    char c;
-
-    get_priorities(&a, &b, &c);
-
-    /*
-    // Get time weight.
-    char *timeWeight = box_read("Time", NUMBER);
-    check_input(timeWeight);
-
-    // Get price weight.
-    char *priceWeight = box_read("Price", NUMBER);
-    check_input(priceWeight);
-
-    // Get emission weight.
-    char *emissionWeight = box_read("Emission", NUMBER);
-    check_input(emissionWeight);
-
-    // Free allocated memory.
-    free(timeWeight);
-    free(priceWeight);
-    free(emissionWeight);
-
-    */
+    // Get user priorities.
+    char price = '\0';
+    char time = '\0';
+    char emission = '\0';
+    get_priorities(&price, &time, &emission);
 
     // get_journey(&startLocation, &endLocation);
 
@@ -66,7 +46,10 @@ int main(void) {
 
     // print_table();
 
-    while (1) {
+    printf("Press any key to exit...");
+    char exit = '\0';
+    while (exit == '\0') {
+        scanf("%c", &exit);
     }
 
     return EXIT_SUCCESS;
--- src/main.h
+++ src/main.h
@@ -7,7 +7,7 @@ typedef enum {
 static const char TRAIN_ROUTES_CSV_PATH[] = "../../data/trainroutes.csv";
 static const char FLIGHT_CSV_PATH[] = "../../data/flight.csv";
 
-static const char journeyInstructions[] = "Andersen's fairy Andersen's fairy Andersen's fairy tales, consisting of 156 stories across nine volumes, have been translated into more than 125 languages. They have become embedded in Western collective consciousness, accessible to children as well as presenting lessons of virtue and resilience in the face of adversity for mature readers.";
+static const char journeyInstructions[] = "A good example of a paragraph contains a topic sentence, details and a conclusion. 'There are many different kinds of animals that live in China. Tigers and leopards are animals that live in China's forests in the north. In the jungles, monkeys swing in the trees and elephants walk through the brush.";
 static const char prioritisationInstructions[] = "Test dsa dsadsa dsad ada dada";
 
 #endif //MAIN_H
