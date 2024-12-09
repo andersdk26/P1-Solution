62ebb7d13f77b3a692425aec52d05a5d73277cfb
Svolle <svolle777@icloud.com>
2024-12-09 13:01:05 +0100

    Added new test.

--- test/tests.c
+++ test/tests.c
@@ -6,11 +6,15 @@
 #include "../src/terminal.h"
 
 void test_add_route();
+
 void test_remove_route();
+
+void test_sort_routes();
 
 int main() {
     test_add_route();
     test_remove_route();
+    test_sort_routes();
 
     printf("Test done\n");
     return 0;
@@ -27,13 +31,26 @@ void test() {
 }
 */
 
+void test_sort_routes() {
+    route_s *routes = NULL;
+    int quantity = 0;
+    get_all_routes("../data/test_routes.csv", tt_train, &routes, &quantity);
+    sort_routes(routes, quantity);
+
+    assert(strcmp(routes[0].origin, "Amsterdam") == 0);
+    assert(strcmp(routes[1].origin, "Berlin") == 0);
+    assert(strcmp(routes[2].origin, "Berlin") == 0);
+    assert(strcmp(routes[3].origin, "London") == 0);
+    assert(strcmp(routes[4].origin, "Oslo") == 0);
+}
+
 void test_add_route() {
     route_s *routes = NULL;
     int routesLength = 0;
 
     append_route(&routes, &routesLength,
-        "or", "de", "or n", "de n",
-        1, 2, 3, 4, tt_plane);
+                 "or", "de", "or n", "de n",
+                 1, 2, 3, 4, tt_plane);
 
     assert(routes != NULL);
     assert(routesLength == 1);
