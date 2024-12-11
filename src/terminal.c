ad98c184caf18b1dba3e652b98a0ec25d20c229d
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2024-12-11 16:18:01 +0100

    Tests added (#22)
    * Tests created
    
    * Added new test.
    
    * test_column_search test created
    
    * test_sort_trips test created
    
    * test_sort_trips test opdated
    
    * set_terminal_mode and exit box updated
    
    ---------
    
    Co-authored-by: Svolle <svolle777@icloud.com>

--- src/terminal.c
+++ src/terminal.c
@@ -224,7 +224,7 @@ void get_priorities(int priorities[3]) {
     set_win_color(wc_bright_white);
 
     // Set terminal settings.
-    set_terminal_mode(ENABLE_WINDOW_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT
+    set_terminal_mode(ENABLE_VIRTUAL_TERMINAL_INPUT
                       ,ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
 
     char c = '\0';
@@ -290,7 +290,7 @@ void read_characters(char *input, const route_s *routes, const int routeQuantity
     input[i] = '\0';
 
     // Prepare terminal for autocomplete.
-    set_terminal_mode(ENABLE_WINDOW_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT
+    set_terminal_mode(ENABLE_VIRTUAL_TERMINAL_INPUT
                       ,ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
 
     while (1) {
---
+++ test/tests.c
@@ -0,0 +1,135 @@
+#include <assert.h>
+#include <string.h>
+
+#include "../src/dataHandling.h"
+#include "../src/general.h"
+#include "../src/terminal.h"
+
+void test_add_route();
+
+void test_remove_route();
+
+void test_sort_routes();
+
+void test_column_search();
+
+void test_sort_trips();
+
+int main() {
+    test_add_route();
+    test_remove_route();
+    test_sort_routes();
+    test_column_search();
+    test_sort_trips();
+
+    printf("Test done\n");
+    return 0;
+}
+
+/*
+void test() {
+    int value = 1;
+    int expected = 2;
+
+    int result = f(value);
+
+    assert(result == expected);
+}
+*/
+
+void test_sort_routes() {
+    route_s *routes = NULL;
+    int quantity = 0;
+    get_all_routes("../data/test_routes.csv", tt_train, &routes, &quantity);
+    sort_routes(routes, quantity);
+
+    assert(strcmp(routes[0].origin, "Amsterdam") == 0);
+    assert(strcmp(routes[1].origin, "Berlin") == 0);
+    assert(strcmp(routes[2].origin, "Berlin") == 0);
+    assert(strcmp(routes[3].origin, "London") == 0);
+    assert(strcmp(routes[4].origin, "Oslo") == 0);
+}
+
+void test_add_route() {
+    route_s *routes = NULL;
+    int routesLength = 0;
+
+    append_route(&routes, &routesLength,
+                 "or", "de", "or n", "de n",
+                 1, 2, 3, 4, tt_plane);
+
+    assert(routes != NULL);
+    assert(routesLength == 1);
+
+    assert(strcmp(routes->origin,"or") == 0);
+    assert(strcmp(routes->destination,"de") == 0);
+    assert(strcmp(routes->originName,"or n") == 0);
+    assert(strcmp(routes->destinationName,"de n") == 0);
+
+    assert(routes->travelTime == 1);
+    assert(routes->emission == 2);
+    assert(routes->price == 3);
+    assert(routes->downtime == 4);
+    assert(routes->transportType == tt_plane);
+}
+
+void test_remove_route() {
+    route_s *routes = memory_allocation(NULL, sizeof(route_s), 0);
+    int routesLength = 1;
+
+    routes->origin = memory_allocation(NULL, 2, 1);
+    routes->destination = memory_allocation(NULL, 2, 1);
+    routes->originName = memory_allocation(NULL, 2, 1);
+    routes->destinationName = memory_allocation(NULL, 2, 1);
+
+    remove_route(&routes, &routesLength, 0);
+
+    assert(routes == NULL);
+    assert(routesLength == 0);
+}
+
+void test_column_search() {
+    route_s *routes = NULL;
+    int quantity = 0;
+    get_all_routes("../data/test_routes.csv", tt_train, &routes, &quantity);
+    sort_routes(routes, quantity);
+    char **str = NULL;
+    int strLength = 0;
+
+    search_first_column("b", &str, &strLength, routes, quantity);
+    assert(strLength == 1);
+    assert(strcmp(str[0], "Berlin") == 0);
+
+    free_string_list(str, strLength, 1);
+    str = NULL;
+    strLength = 0;
+
+    search_second_column("berlin", "v", &str, &strLength, routes, quantity);
+    assert(strLength == 1);
+    assert(strcmp(str[0], "Vienna") == 0);
+
+    free_string_list(str, strLength, 1);
+    free_route_list(routes, quantity, 1);
+}
+
+void test_sort_trips() {
+    route_s *routes = NULL;
+    int quantity = 0;
+    get_all_routes("../data/test_routes.csv", tt_train, &routes, &quantity);
+    sort_routes(routes, quantity);
+    int priorities[] = {p_price, p_time, p_emission};
+
+    sort_trips(routes, quantity, priorities);
+    assert(strcmp(routes[0].origin, "Amsterdam") == 0);
+    assert(strcmp(routes[1].origin, "London") == 0);
+    assert(strcmp(routes[2].origin, "Oslo") == 0);
+
+    priorities[0] = p_time;
+    priorities[1] = p_price;
+    sort_trips(routes, quantity, priorities);
+    assert(strcmp(routes[0].origin, "Oslo") == 0);
+    assert(strcmp(routes[1].origin, "Amsterdam") == 0);
+    assert(strcmp(routes[2].origin, "Berlin") == 0);
+
+    free_route_list(routes, quantity, 1);
+}
